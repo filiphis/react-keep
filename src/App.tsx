46aa4db8e774d8905f0be8c7312cb7a416838534
Luiz Silveira <l.filiphis@hotmail.com>
2020-08-07 00:55:51 -0300

    ✨ Criando o primeiro class component -> ListaDeNotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import React from 'react';
+import ListaDeNotas from './components/ListaDeNotas';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <section>
+      <form action="">
+        <input type="text" placeholder="Titulo" />
+        <textarea placeholder="Escreva sua nota" />
+        <button type="button">Criar nota</button>
+      </form>
+
+      <ListaDeNotas />
+    </section>
   );
 }
 
---
+++ src/components/ListaDeNotas.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+export default class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Titulo</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Titulo</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Titulo</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    );
+  }
+}
